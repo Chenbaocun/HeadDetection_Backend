504c184b5846a6063ac3042771dfdc234dcd46c5
Chenbaocun <1066134123@qq.com>
2019-06-12 10:37:36 +0800

    test image_detect

--- Home/image_detect.py
+++ Home/image_detect.py
@@ -117,6 +117,8 @@ def image_detect(input_path,output_path,filename,username):
     plt.margins(0, 0)
     filename=filename.split('.')[0]
     plt.savefig(output_path+filename+".jpg")
+    print("图片计算结束")
+    AbnormalImage.objects.filter(username=username,filename=filename).update(hascalculated=1)
     undetected_image=AbnormalImage.objects.filter(hascalculated=0)
     if undetected_image:
         for image in undetected_image:
